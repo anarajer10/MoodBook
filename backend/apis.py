5179ade62fbc01c184dfda388a9cdc02bd7ad249
luciagl03 <lucia.gallar@hotmail.com>
2025-08-28 19:04:05 +0200

    Modificacion archivos apis.py y emotions.py

--- backend/apis.py
+++ backend/apis.py
@@ -1,3 +1,65 @@
 #LLamadas a las APIs públicas
 #-Google Books
 #-OpenLibrary
+
+import requests #Llamadas HTTP a las APIs
+
+#Método de llamada a Google Books
+def get_googleBooks(search, maxResults=3):
+    #La f delante de la url convierte la cadena en string (de los recursos de Volumen)
+    url = f'https://www.googleapis.com/books/v1/volumes?q={search}&maxResults={maxResults}'
+    result = requests.get(url)
+    books = []
+
+    if result.status_code == 200: #Si el estado de la API es correcto "OK"
+        data = result.json()
+        for item in data.get('items', []): #REVISAR
+            volume = item.get('volumeInfo', {}) 
+            #Lista de datos (Título, Autores, Descripción, Portada)
+            books.append({ #Cada elemento se añade al final de la lista
+                #Título-->Title
+                "title": volume.get('title'),
+                #Autor/es-->Authors
+                "authors": volume.get('authors', []),
+                #Sinopsis/descripción-->description
+                "description": volume.get('description', "Descripción no disponible"),
+                #Portada/imagen-->imageLinks(thumbnail)
+                "thumbnail": volume.get('imageLinks', {}).get('thumbnail')     
+            })
+    
+    return books
+
+#Método de llamada a Open Library
+def get_openLibrary(search, maxResults=3):
+    #La f delante de la url convierte la cadena en string (de los recursos de Volumen)
+    url = f'https://openlibrary.org/search.json?q={search}&limit={maxResults}'
+    result = requests.get(url)
+    books = []
+
+    if result.status_code == 200: #Si el estado de la API es correcto "OK"
+        data = result.json()
+        for item in data.get('docs', []):
+            #Identificador de la portada-->cover_i
+            cover_id = item.get('cover_id')
+            work_key = item.get('key')
+
+            #Descripción del libro
+            description = "Descripción no disponible"
+            if work_key:
+                work_url=f"https://openlibrary.org{work_key}.json/"
+                work_data=requests.get(work_url).json()
+                desc=work_data.get("description","")
+                if isinstance(desc, dict):
+                    description=desc.get("value", "Descripción no disponible")
+                elif isinstance(desc, str):
+                    description=desc
+            
+            #Lista de datos
+            books.append({
+                "title": item.get('title'),
+                "authors": item.get('author_name', []),
+                "description": description,
+                "thumbnail": f"https://covers.openlibrary.org/b/id/{cover_id}-M.jpg" if cover_id else None
+            })
+
+    return books
--- backend/emotions.py
+++ backend/emotions.py
@@ -1,1 +1,31 @@
 #Funciones específicas sobre las emociones detectadas
+from textblob import TextBlob
+
+
+def franjas(t): #Hace falta que se cambie para que lo ponga el usuario
+    t = TextBlob("").translate(from_lang='es', to='en')
+    polarity = t.sentiment.polarity
+    
+    if (polarity > -1 and polarity < -0.66):
+        return "muy malo" #aquí va la lista de libros correspondiente
+    
+    elif (polarity > -0.65 and polarity < -0.25):
+        return "malo" #aquí va la lista de libros correspondiente
+    
+    elif (polarity > -0.24 and polarity < 0.25):
+        return "neutral" #aquí va la lista de libros correspondiente
+    
+    elif (polarity > 0.26 and polarity < 0.66):
+        return "bueno" #aquí va la lista de libros correspondiente
+    
+    elif (polarity > 0.67 and polarity < 1):
+        return "muy_bueno" #aquí va la lista de libros correspondiente
+    
+    else:
+        return "nada"
+
+#-1/-0.66 muy malo
+#-0.65/-0.25 malo
+#-0.24/0.25 neutral
+#0.26/0.66 bueno
+#0.67/1 muy bueno
