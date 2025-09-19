371deb49f730d8e4c747a427be4d739ebb8a2e14
anarajer10 <anarajer@correo.ugr.es>
2025-09-19 19:15:58 +0200

    Finalizacion del backend

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,36 @@
 #Montar el framework (Flask)
+from flask import Flask, request, jsonify
+from apis import get_googleBooks, get_openLibrary
+from emotions import franjas
+
+app = Flask(__name__)
+
+@app.route("/")
+def formulario():
+    return """
+        <h1>Buscador de libros segun emociones<h1>
+        <form action="/submit" method="post">
+            <label for="texto">Escribe un texto:</label><br>
+            <textarea name="texto" rows="4" cols="50" ></textarea><br><br>
+            <button type="submit">Analizar</button>
+        </form>
+        """
+
+
+@app.route("/submit", methods=['POST'])
+def books():
+#Procesa los datos del formulario
+    texto = request.form.get("texto") #Se recoge el texto del usuario
+    emocion = franjas(texto)
+    libros_gb = get_googleBooks(emocion, maxResults=3)
+    libros_ol = get_openLibrary(emocion, maxResults=3)
+    
+    return jsonify({
+        "texto": texto,
+        "emocion_detectada": emocion,
+        "gb_books": libros_gb,
+        "ol_books": libros_ol
+    })
+
+if __name__ == '__main__':
+    app.run(debug=True)     #Ejecuta la app en modo debug
--- backend/emotions.py
+++ backend/emotions.py
@@ -1,31 +1,46 @@
 #Funciones específicas sobre las emociones detectadas
 from textblob import TextBlob
+from deep_translator import GoogleTranslator
+from langdetect import detect
 
+def franjas(t):  #t --> texto 
+    """
+    Recibe un texto en español/inglés, lo traduce si hace falta y analiza el tipo de sentimiento
+    """
+    try:
+        detected_lang = detect(t)
+        if (detected_lang == 'es' ):
+            traduccion = GoogleTranslator(source='es', target='en').translate(t) #Se traduce
+            blob = TextBlob(traduccion)
+        elif detected_lang == "en":
+            blob = TextBlob(t)
+        else:
+            return "Idioma no compatible, por favor introducir en español o inglés"
 
-def franjas(t): #Hace falta que se cambie para que lo ponga el usuario
-    t = TextBlob("").translate(from_lang='es', to='en')
-    polarity = t.sentiment.polarity
-    
-    if (polarity > -1 and polarity < -0.66):
-        return "muy malo" #aquí va la lista de libros correspondiente
-    
-    elif (polarity > -0.65 and polarity < -0.25):
-        return "malo" #aquí va la lista de libros correspondiente
-    
-    elif (polarity > -0.24 and polarity < 0.25):
-        return "neutral" #aquí va la lista de libros correspondiente
-    
-    elif (polarity > 0.26 and polarity < 0.66):
-        return "bueno" #aquí va la lista de libros correspondiente
-    
-    elif (polarity > 0.67 and polarity < 1):
-        return "muy_bueno" #aquí va la lista de libros correspondiente
-    
-    else:
-        return "nada"
-
-#-1/-0.66 muy malo
-#-0.65/-0.25 malo
-#-0.24/0.25 neutral
-#0.26/0.66 bueno
-#0.67/1 muy bueno
+        polarity = blob.sentiment.polarity
+        
+        if (polarity > -1 and polarity < -0.66):
+            return "muy malo" #aquí va la lista de libros correspondiente
+        
+        elif (polarity > -0.65 and polarity < -0.25):
+            return "malo" #aquí va la lista de libros correspondiente
+        
+        elif (polarity > -0.24 and polarity < 0.25):
+            return "neutral" #aquí va la lista de libros correspondiente
+        
+        elif (polarity > 0.26 and polarity < 0.66):
+            return "bueno" #aquí va la lista de libros correspondiente
+        
+        elif (polarity > 0.67 and polarity < 1):
+            return "muy bueno" #aquí va la lista de libros correspondiente
+        
+        else:
+            return "nada"
+        
+    except Exception as e:
+        print("Error en análisis: ", e)
+    #-1/-0.66 muy malo
+    #-0.65/-0.25 malo
+    #-0.24/0.25 neutral
+    #0.26/0.66 bueno
+    #0.67/1 muy bueno
